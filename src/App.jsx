eff86457140d3d43514e7328c3d8bfb0de7cda3c
Julio28012020 <aldanaroque2001@gmail.com>
2026-04-23 16:09:58 -0500

    feat: instalacion de tailwind, react-router y limpieza inicial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-100 min-h-screen text-center p-10">
+      <h1 className="text-4xl text-green-800 font-bold">¡Zenith Bonsai con Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
